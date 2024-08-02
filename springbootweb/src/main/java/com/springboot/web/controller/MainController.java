7ab38cb3e80037bc6fe822b25d7adcdd92e794ba
chandan kumar <145868696+chandan2544@users.noreply.github.com>
2024-08-02 17:25:27 +0530

    Add files via upload

---
+++ springbootweb/src/main/java/com/springboot/web/controller/MainController.java
@@ -0,0 +1,22 @@
+package com.springboot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/home")
+	public String home(){
+		System.out.println("this is a home page");
+		return "home";
+	
+	}
+
+	@RequestMapping("/contact")
+	public String contact() {
+		System.out.println("this is a contact page");
+		return "contact";
+	}
+	
+}
